704134581d435a0c652ca4af51d0c9b8b6670ba0
abdelrahmanbadr <abdelrahmanbadr.it@gmail.com>
2018-05-02 17:11:58 +0200

    adding router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { BeerService } from './Services/beer.service';
 import { BreweryService } from './Services/brewery.service';
 import {HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './components/app/app.component';
 import { BeersComponent } from './components/beers/beers.component';
 
+const appRoutes: Routes = [
+  { path: '', component: BeersComponent },
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +20,8 @@ import { BeersComponent } from './components/beers/beers.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [BreweryService,BeerService],
   bootstrap: [AppComponent]
